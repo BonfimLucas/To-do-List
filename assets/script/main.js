b90fba3ea38fbb0df5967e2ee4a017ce6c2f2ac1
Lucas <lucasbonfim604@gmail.com>
2024-06-14 13:01:46 -0300

    add envio de tarefa ao pressionar enter

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,9 +1,20 @@
 function main(){
 
-    let resultado = document.querySelector('#resultado')
+    let resultado = document.querySelector('#tarefas')
     let enviaTarefa = document.querySelector('#envia-tarefa')
-
     
+    function criaTarefa(textoInput){   
+
+        console.log(textoInput);
+
+    }
+
+    enviaTarefa.addEventListener('keypress', function(e){
+        if(!enviaTarefa.value) console.log('campo vazio');
+
+        if (e.which == 13) criaTarefa(enviaTarefa.value) 
+    })
+
 }
 
 main()
